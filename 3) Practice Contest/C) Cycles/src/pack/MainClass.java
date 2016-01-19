b2a8664577031ffaac3f66eab595f61eb13ae0f9
Aditya Mishra <adityam.rtm@gmail.com>
2016-01-19 16:31:25 +0530

    slightly modified solution for cycles

--- 3) Practice Contest/C) Cycles/src/pack/MainClass.java
+++ 3) Practice Contest/C) Cycles/src/pack/MainClass.java
@@ -6,24 +6,40 @@ import java.util.Scanner;
 
 public class MainClass {
 
+	public static int n, k, edges[][], degree[];
+
+	public static int getPathCount(int i) {
+		int solution = 0;
+
+		return solution;
+	}
+
 	public static void main(String[] args) throws Exception {
 
 		Scanner scan = new Scanner(new File("input.in"));
 		PrintWriter out = new PrintWriter(new File("output.out"));
 
 		int T = scan.nextInt();
-		int n,k,edges[][];
+		int count[];
 		for (int cT = 1; cT <= T; cT++) {
 			n = scan.nextInt();
 			k = scan.nextInt();
-			
+
+			degree = new int[n];
 			edges = new int[k][2];
-			
-			for(int i=0;i<k;i++){
-				edges[i][0] = scan.nextInt();
-				edges[i][1] = scan.nextInt();
+			count = new int[1 << k];
+
+			for (int i = 0; i < k; i++) {
+				edges[i][0] = scan.nextInt() - 1;
+				edges[i][1] = scan.nextInt() - 1;
 			}
+
+			for (int i = 1; i < 1 << k; i++)
+				count[i] = getPathCount(i);
 		}
+
+		scan.close();
+		out.close();
 	}
 
 }
