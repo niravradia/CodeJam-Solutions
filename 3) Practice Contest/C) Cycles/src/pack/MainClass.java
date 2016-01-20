80a97ef0818f1286d33f7745af190f794ee03fac
Aditya Mishra <adityam.rtm@gmail.com>
2016-01-20 18:42:44 +0530

    added more structure to the getPathCount() function

--- 3) Practice Contest/C) Cycles/src/pack/MainClass.java
+++ 3) Practice Contest/C) Cycles/src/pack/MainClass.java
@@ -2,14 +2,46 @@ package pack;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainClass {
 
 	public static int n, k, edges[][], degree[];
+	public static ArrayList<Integer> vtoe[];
 
-	public static int getPathCount(int i) {
+	public static int getPathCount(int s) {
 		int solution = 0;
+
+		int l = 0, st = s;
+		while (st > 0) {
+			l += st & 1;
+			st = st >> 1;
+		}
+		int activeEdge[] = new int[l];
+		int c = 0;
+		for (int i = 1, index = 0; i <= s; i = i << 1, index++)
+			if ((i & s) > 0) {
+				activeEdge[c++] = index;
+			}
+
+		Arrays.fill(degree, 0);
+		for (int i = 0; i < l; i++) {
+			if (++degree[edges[activeEdge[i]][0]] > 2
+					|| ++degree[edges[activeEdge[i]][1]] > 2)
+				return 0;
+		}
+
+		boolean checkedForCycle[] = new boolean[l];
+
+		for (int i = 0; i < l; i++)
+			if (!checkedForCycle[i]) {
+				checkedForCycle[i] = false;
+				while (degree[edges[activeEdge[i]][0]] == 2) {
+
+				}
+			}
 
 		return solution;
 	}
@@ -26,14 +58,19 @@ public class MainClass {
 			k = scan.nextInt();
 
 			degree = new int[n];
+			for (int i = 0; i < n; i++)
+				vtoe[i] = new ArrayList<Integer>();
 			edges = new int[k][2];
 			count = new int[1 << k];
 
 			for (int i = 0; i < k; i++) {
 				edges[i][0] = scan.nextInt() - 1;
 				edges[i][1] = scan.nextInt() - 1;
-			}
 
+				vtoe[edges[i][0]].add(i);
+				vtoe[edges[i][1]].add(i);
+			}
+			Arrays.fill(vtoe, -1);
 			for (int i = 1; i < 1 << k; i++)
 				count[i] = getPathCount(i);
 		}
@@ -41,5 +78,4 @@ public class MainClass {
 		scan.close();
 		out.close();
 	}
-
 }
